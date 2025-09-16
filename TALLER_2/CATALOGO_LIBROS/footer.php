ece12731ca3d1ab62f15740d2885d3e83eea233d
Rintaro9929 <84785751+Rintaro9929@users.noreply.github.com>
2025-09-15 19:21:48 -0500

    Delete TALLER_2 directory

--- TALLER_2/CATALOGO_LIBROS/footer.php
+++
@@ -1,3 +0,0 @@
-<footer style="background: #222; color: #fff; text-align: center; padding: 1em 0; margin-top: 2em;">
-    &copy; <?php echo date('Y'); ?> Catálogo de Libros. Todos los derechos reservados.
-</footer>

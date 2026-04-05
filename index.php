ce87c494883354671a58ce5b810e4593c6374c52
Rintaro9929 <84785751+Rintaro9929@users.noreply.github.com>
2026-04-05 15:57:14 -0500

    ok

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+<body>
+
+<h1>Bienvenido a mi laboratorio</h1>
+
+<ul>
+    <li><a href="practica1.php">Ir a Practica#1</a></li>
+</ul>
+
+</body>
+</html>

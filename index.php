247446c10a5d85aebefff59f38d8dcb47b451d9c
Rintaro9929 <84785751+Rintaro9929@users.noreply.github.com>
2026-04-05 16:08:55 -0500

    listo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="estilo-index.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Laboratorio PHP</h1>
+
+    <ul>
+        <li><a href="Trabajos/practica1.php">Práctica 1</a></li>
+    </ul>
+</div>
+
+</body>
+</html>

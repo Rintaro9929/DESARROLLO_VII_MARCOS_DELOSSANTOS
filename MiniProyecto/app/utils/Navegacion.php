b98a8a22455989ce5cd43bb45b10ebed9a4a0936
Rintaro9929 <84785751+Rintaro9929@users.noreply.github.com>
2026-06-14 16:36:29 -0500

    l

--- MiniProyecto/app/utils/Navegacion.php
+++ MiniProyecto/app/utils/Navegacion.php
@@ -2,9 +2,8 @@
 
 class Navegacion
 {
-    public static function volverMenu($url)
+    public static function volverMenu(string $ruta): string
     {
-        return "<a href='$url'>Volver al menú principal</a>";
+        return '<a href="' . $ruta . '" class="btn-volver">Volver al menú principal</a>';
     }
 }
-?>

1c541b41477282b23a67f3c83d71002ebbf76ae1
Rintaro9929 <84785751+Rintaro9929@users.noreply.github.com>
2026-06-14 16:38:57 -0500

    l

--- MiniProyecto/public/Problema.php
+++ MiniProyecto/public/Problema.php
@@ -1,6 +1,9 @@
 <?php
 
+// Clase abstracta que define un problema genérico
 abstract class Problema
 {
+    // Método abstracto que debe implementar la clase hija
+    // Debe contener la lógica para resolver el problema
     abstract public function resolver();
 }
